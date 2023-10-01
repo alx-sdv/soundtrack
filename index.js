b304d8234a9afac2580f363f2fdff20a5ce01d95
alx-sdv <30608516+alx-sdv@users.noreply.github.com>
2023-10-01 18:44:02 +0300

    1 Oct 2023 updates

---
+++ index.js
@@ -0,0 +1,21 @@
+document.body.onload = async function() {
+	const resp = await fetch("data.json");
+	const data = await resp.json();
+	let table = document.getElementById("view");
+	let typeColors = new Map();
+	typeColors.set("artist", "is-primary is-light")
+	typeColors.set("playlist", "is-light")
+	typeColors.set("radio", "is-info is-light")
+	typeColors.set("noise", "is-dark")
+	for (const i in data.reverse()) {
+		let row = table.insertRow();
+		let cAdded = row.insertCell();
+		let cType = row.insertCell();
+		let cName = row.insertCell();
+		let cSource = row.insertCell();
+		cAdded.innerHTML = data[i].added;
+		cType.innerHTML = `<span class="tag ${typeColors.get(data[i].type)}">${data[i].type}</span>`;
+		cName.innerHTML = `<a href="${data[i].link}" target="_blank">${data[i].name}</a>`;
+		cSource.innerHTML = `<span class="tag is-white">${data[i].link.includes('youtube') ? 'youtube' : 'other'}</span>`;
+	}
+}
